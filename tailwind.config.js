3dfec0d1f0395312e36470d67925df0e090478fb
abinjoseph2050 <abinjoseph2050@gmail.com>
2024-03-01 14:12:23 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    // extend: {
+    //   backgroundColor: {
+    //     'random': '#4880fb',
+    //     'red': '#ff0000',
+    //     'green': '#00ff00',
+    //     'blue': '#0000ff',
+    //     'yellow': '#ffff00',
+    //   },
+    // },
   },
   plugins: [],
 }
